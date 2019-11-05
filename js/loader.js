dfe4fddb84f7735c2f66cca37ee5c46a4750dae9
ARodriguezJr <31947104+ARodriguezJr@users.noreply.github.com>
2019-11-04 18:44:33 -0800

    Test div element created to populate item page

--- js/loader.js
+++ js/loader.js
@@ -13,6 +13,13 @@ function loadItems(){
       var itemPrice = itemRaw.slice(dollarIndex);
       
       document.getElementById("demo").innerHTML = itemName;
+
+      var itemCard = document.createElement("div");
+      itemCard.className = "item-tile";
+      // Add attributes and html to card 
+      itemCard.innerText = "CARD";
+      // append to list
+      document.body.appendChild(itemCard); // Maybe use a different append fucntion here
       /*var item = {
         name: itemName,
         price: itemPrice
