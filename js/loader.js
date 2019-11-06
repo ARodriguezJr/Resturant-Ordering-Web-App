267467c9397f346b2b2204a3d1277f23bed1c912
ARodriguezJr <31947104+ARodriguezJr@users.noreply.github.com>
2019-11-06 12:42:45 -0800

    Added innerHTML formatting; Ready to laod in items

--- js/loader.js
+++ js/loader.js
@@ -16,15 +16,30 @@ function loadItems(){
 
       var itemCard = document.createElement("div");
       itemCard.className = "item-tile";
+      itemCard.innerHTML = "<div class='item-tile-photo'>" + "<img src='https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png'/>" +  "</div><div class='item-tile-description'>" + "<h1>White Ceramic</h1><p>Beautiful ceramic plates useful for any occaison </p></div>";
+      //itemCard.innerHTML = <div class="item-tile-photo"><img src="icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"/></div><div class="item-tile-description"><h1>White Ceramic</h1><p>Beautiful ceramic plates useful for any occaison </p></div>;
+
+      /*itemCard.innerHTML = 
+      "<div class="item-tile">" +
+      "<div class="item-tile-photo">" +
+      "<img src="icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png"/>" +
+      "</div><div class="item-tile-description">" +
+      "<h1>White Ceramic</h1>" +
+      "<p>Beautiful ceramic plates useful for any occaison </p>";*/
+      
+      /*** WORKING CODE UNDER HERE
+      itemCard.className = "item-tile";
       // Add attributes and html to card 
       itemCard.innerText = "CARD";
       // append to list
       document.body.appendChild(itemCard); // Maybe use a different append fucntion here
+      WORKING CODE ABOVE HERE
+      */ 
       /*var item = {
         name: itemName,
         price: itemPrice
       }*/
-
+      document.body.appendChild(itemCard); // Maybe use a different append fucntion here
       //itemArray.push(item);
     }
   };
